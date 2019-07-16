0e1752229fa8066586e87d98830f5fa2450909f4
alejandro-salinas <salinas.asalinas@gmail.com>
2019-07-16 14:19:46 -0700

    Set up Parse client.

---
+++ app/src/main/java/com/codepath/travelapp/ParseApp.java
@@ -0,0 +1,21 @@
+package com.codepath.travelapp;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Set up configuration
+        final Parse.Configuration configuration = new Parse.Configuration.Builder(this)
+                .applicationId("travel-app-fbu-2019")
+                .clientKey("westlake-bestlake")
+                .server("http://travel-app-fbu.herokuapp.com/parse")
+                .build();
+        Parse.initialize(configuration);
+    }
+}
