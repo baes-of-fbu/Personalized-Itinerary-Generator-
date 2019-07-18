447cfb4a56bdc16567f627ff72cfb990622b6359
Emily Gelb <egelb@sas.upenn.edu>
2019-07-17 17:13:47 -0700

    Trip details view

--- app/src/main/java/com/codepath/travelapp/fragments/ComposeFragment.java
+++ app/src/main/java/com/codepath/travelapp/fragments/ComposeFragment.java
@@ -1,19 +1,30 @@
 package com.codepath.travelapp.fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.codepath.travelapp.MainActivity;
 import com.codepath.travelapp.R;
 
 public class ComposeFragment extends Fragment {
 
     private final String TAG = "ComposeFragment";
+
+    private EditText etTripName;
+    private EditText etStartDate;
+    private EditText etEndDate;
+    private EditText etBudget;
+    private EditText etCity;
+    private Button btnGenerate;
 
     @Nullable
     @Override
@@ -25,5 +36,27 @@ public class ComposeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        etTripName = view.findViewById(R.id.etTripName);
+        etStartDate = view.findViewById(R.id.etStartDate);
+        etEndDate = view.findViewById(R.id.etEndDate);
+        etBudget = view.findViewById(R.id.etBudget);
+        etCity = view.findViewById(R.id.etCity);
+        btnGenerate = view.findViewById(R.id.btnGenerate);
+
+        btnGenerate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new TripDetailsFragment();
+
+                Bundle bundle = new Bundle();
+                //bundle.putSerializable("Post", post);
+                fragment.setArguments(bundle);
+
+                MainActivity.fragmentManager.beginTransaction()
+                        .replace(R.id.flContainer, fragment)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/codepath/travelapp/fragments/TripDetailsFragment.java
@@ -0,0 +1,29 @@
+package com.codepath.travelapp.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.codepath.travelapp.R;
+
+public class TripDetailsFragment extends Fragment {
+
+    private final String TAG = "ComposeFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_trip_details,container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
