0785eb5f3b3eb55e0047ade178e92530f6b5761b
Britteny Okorom-Achuonye <bokoromachuonye@college.harvard.edu>
2019-07-17 21:56:46 -0700

    More UI for item post.

--- app/src/main/java/com/codepath/travelapp/TripAdapter.java
+++ app/src/main/java/com/codepath/travelapp/TripAdapter.java
@@ -12,8 +12,11 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.codepath.travelapp.Models.Trip;
 import com.parse.ParseFile;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,9 +41,18 @@ public class TripAdapter extends RecyclerView.Adapter<TripAdapter.ViewHolder> {
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Trip trip = (Trip) trips.get(position);
         holder.tvTripBudget.setText(trip.getBudget().toString());
-        holder.tvTripDates.setText(trip.getStartDate().toString());
+        holder.tvTripDates.setText(trip.getNumDays().toString());
         holder.tvTripName.setText(trip.getName());
+        holder.tvUsername.setText(trip.getOwner().getUsername());
 
+
+        if (trip.getOwner().get("profileImage") != null) {
+            ParseFile image = (ParseFile) trip.getOwner().get("profileImage") ;
+            Glide.with(context)
+                    .load(image.getUrl())
+                    .apply(RequestOptions.circleCropTransform())
+                    .into(holder.ivProfileImage);
+        }
         if (trip.get("image") != null) {
             ParseFile image = (ParseFile) trip.get("image");
             Glide.with(context)
@@ -60,6 +72,8 @@ public class TripAdapter extends RecyclerView.Adapter<TripAdapter.ViewHolder> {
         private TextView tvTripDates;
         private ImageView ivTripImage;
         private TextView tvTripName;
+        private TextView tvUsername;
+        private ImageView ivProfileImage;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -67,6 +81,8 @@ public class TripAdapter extends RecyclerView.Adapter<TripAdapter.ViewHolder> {
             tvTripBudget = itemView.findViewById(R.id.tvTripBudget);
             tvTripName = itemView.findViewById(R.id.tvTripName);
             ivTripImage = itemView.findViewById(R.id.ivTripImage);
+            tvUsername = itemView.findViewById(R.id.tvUsername);
+            ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
         }
     }
     public void clear(){
