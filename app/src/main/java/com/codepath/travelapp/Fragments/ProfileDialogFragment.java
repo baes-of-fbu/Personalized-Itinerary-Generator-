fc9204555e6cc5ca7bfb7eece7ab490829fc7048
Britteny Okorom-Achuonye <bokoromachuonye@college.harvard.edu>
2019-07-26 17:35:37 -0700

    Profile Edit Profile additions

--- app/src/main/java/com/codepath/travelapp/Fragments/ProfileDialogFragment.java
+++ app/src/main/java/com/codepath/travelapp/Fragments/ProfileDialogFragment.java
@@ -8,7 +8,6 @@ import android.graphics.Point;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.Display;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -17,36 +16,47 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
-import com.codepath.travelapp.Activities.LoginActivity;
-import com.codepath.travelapp.Activities.SignUpActivity;
+import com.codepath.travelapp.Activities.MainActivity;
 import com.codepath.travelapp.Models.User;
 import com.codepath.travelapp.R;
+import com.parse.ParseException;
 import com.parse.ParseFile;
-import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 
 import static android.app.Activity.RESULT_OK;
+import static com.parse.ParseUser.getCurrentUser;
 
 public class ProfileDialogFragment extends DialogFragment {
 
+    private User user;
     private Button imageBtn;
     private ImageView ivProfileImage;
-    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
-    public ProfileDialogFragment() {
-    }
+    private EditText etBio;
+    private Spinner spHomeState;
+    private String newBio;
+    private String newState;
+    private ParseFile newImage;
+    private final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    private Bitmap selectedImage;
 
-    public static ProfileDialogFragment newInstance() {
+    public ProfileDialogFragment() { }
+
+    static ProfileDialogFragment newInstance() {
         ProfileDialogFragment frag = new ProfileDialogFragment();
         return frag;
     }
@@ -65,17 +75,20 @@ public class ProfileDialogFragment extends DialogFragment {
         Display display = window.getWindowManager().getDefaultDisplay();
         display.getSize(size);
         // Set the width of the dialog proportional to 75% of the screen width
-        window.setLayout((int) (size.x), WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setLayout((size.x), WindowManager.LayoutParams.WRAP_CONTENT);
         window.setGravity(Gravity.CENTER);
         // Call super onResume after sizing
         super.onResume();
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         imageBtn = view.findViewById(R.id.imageBtn);
         ivProfileImage = view.findViewById(R.id.ivProfileImage);
+        etBio = view.findViewById(R.id.etBio);
+        spHomeState = view.findViewById(R.id.spHomeState);
+        Button applyBtn = view.findViewById(R.id.applyBtn);
 
         imageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,13 +96,38 @@ public class ProfileDialogFragment extends DialogFragment {
                 onLaunchGallery(view);
             }
         });
+
+        applyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newBio = etBio.getText().toString();
+                newState = spHomeState.getSelectedItem().toString();
+                newImage = conversionBitmapParseFile(selectedImage);
+
+                // Check to see all fields are entered
+                if (newBio.length() == 0) {
+                    Toast.makeText(getContext(), "Please enter a bio", Toast.LENGTH_SHORT).show();
+                }else if(newState.length() > 2) {
+                    Toast.makeText(getContext(), "Please select a State", Toast.LENGTH_SHORT).show();
+                }else if(newImage == null) {
+                    Toast.makeText(getContext(), "Please select a Profile picture", Toast.LENGTH_SHORT).show();
+                }else{
+                    ApplyChanges(newBio, newState, newImage);
+                }
+
+
+            }
+        });
+        SetViews();
     }
-    public void onLaunchGallery(View view) {
+
+    private void onLaunchGallery(View view) {
         Intent photoPickerIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         if (photoPickerIntent.resolveActivity(getContext().getPackageManager()) != null) {
             startActivityForResult(photoPickerIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
         }
     }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
@@ -97,7 +135,7 @@ public class ProfileDialogFragment extends DialogFragment {
                 final Uri imageUri = data.getData();
                 File photoFile = new File(getRealPathFromURI(getContext(), imageUri));
                 try {
-                    Bitmap selectedImage = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), imageUri);
+                   selectedImage = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), imageUri);
                     if (selectedImage == null) {
                         ivProfileImage.setVisibility(View.GONE);
                     } else {
@@ -115,7 +153,8 @@ public class ProfileDialogFragment extends DialogFragment {
             }
         }
     }
-    public String getRealPathFromURI(Context context, Uri contentUri) {
+
+    private String getRealPathFromURI(Context context, Uri contentUri) {
         Cursor cursor = null;
         try {
             String[] proj = {MediaStore.Images.Media.DATA};
@@ -130,11 +169,41 @@ public class ProfileDialogFragment extends DialogFragment {
         }
     }
 
-    public static ParseFile conversionBitmapParseFile(Bitmap imageBitmap){
+    private static ParseFile conversionBitmapParseFile(Bitmap imageBitmap){
         ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
         imageBitmap.compress(Bitmap.CompressFormat.PNG,100,byteArrayOutputStream);
         byte[] imageByte = byteArrayOutputStream.toByteArray();
         ParseFile parseFile = new ParseFile("image_file.png",imageByte);
         return parseFile;
+    }
+
+    private void SetViews () {
+        user = (User) User.getCurrentUser();
+        etBio.setText(user.getBio());
+        if (user.getProfileImage() != null) {
+            ParseFile image = user.getProfileImage();
+            Glide.with(getContext())
+                    .load(image.getUrl())
+                    .apply(RequestOptions.circleCropTransform())
+                    .into(ivProfileImage);
+        }
+    }
+
+    private void ApplyChanges(String newBio, String newState, ParseFile newImage) {
+        user.setBio(newBio);
+        user.setHomeState(newState);
+        user.setProfileImage(newImage);
+
+        user.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                SidebarFragment fragment = new SidebarFragment();
+                MainActivity.fragmentManager.beginTransaction()
+                        .replace(R.id.flContainer, fragment)
+                        .addToBackStack(null)
+                        .commit();
+                dismiss();
+            }
+        });
     }
 }
