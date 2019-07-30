d0d74a12555b1fa1ce94d212df4b8bc3105e569e
Britteny Okorom-Achuonye <bokoromachuonye@college.harvard.edu>
2019-07-29 17:49:51 -0700

    Started Opening Slideshow

--- app/src/main/java/com/codepath/travelapp/Activities/OpeningActivity.java
+++ app/src/main/java/com/codepath/travelapp/Activities/OpeningActivity.java
@@ -2,21 +2,49 @@ package com.codepath.travelapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 import com.codepath.travelapp.R;
 import com.parse.ParseUser;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class OpeningActivity extends AppCompatActivity {
+    private ImageSwitcher imageSwitcher;
+    private int[] gallery = {R.drawable.seattle1, R.drawable.seattle2, R.drawable.seattle3, R.drawable.seattle4, R.drawable.seattle5, R.drawable.seattle6, R.drawable.seattle7, R.drawable.seattle8, R.drawable.seattle9, R.drawable.seattle10};
+    private int position = 0;
+    private Timer timer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opening);
+        imageSwitcher = findViewById(R.id.imageSwitcher);
+        imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+
+            public View makeView()
+            {
+                return new ImageView(OpeningActivity.this);
+            }
+        });
+
+        // Set animations http://danielme.com/2013/08/18/diseno-android-transiciones-entre-activities/
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        Animation fadeOut = AnimationUtils.loadAnimation(this, R.anim.fade_out);
+        imageSwitcher.setInAnimation(fadeIn);
+        imageSwitcher.setOutAnimation(fadeOut);
         ParseUser currentUser = ParseUser.getCurrentUser();
         if (currentUser != null) {
             Intent intent = new Intent(OpeningActivity.this, MainActivity.class);
@@ -40,5 +68,32 @@ public class OpeningActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    public void start()
+    {
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+
+            public void run() {
+                // avoid exception: "Only the original thread that created a view hierarchy can touch its views"
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        imageSwitcher.setImageResource(gallery[position]);
+                        position++;
+                        if (position == 10)
+                        {
+                            position = 0;
+                        }
+                    }
+                });
+            }
+
+        }, 0, 2500);
+
+    }
+
+    public void stop()
+    {
+        timer.cancel();
     }
 }
