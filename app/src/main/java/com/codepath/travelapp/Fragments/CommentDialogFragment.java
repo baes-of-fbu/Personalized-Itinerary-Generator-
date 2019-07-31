2e93902385116eefbd74e0cf6c1e9b12341b990f
Emily Gelb <egelb@sas.upenn.edu>
2019-07-31 16:29:13 -0700

    Created comment dialog fragment

---
+++ app/src/main/java/com/codepath/travelapp/Fragments/CommentDialogFragment.java
@@ -0,0 +1,25 @@
+package com.codepath.travelapp.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.codepath.travelapp.R;
+
+public class CommentDialogFragment extends DialogFragment {
+
+    public CommentDialogFragment() {}
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_comment_dialog, container, false);
+    }
+
+
+}
